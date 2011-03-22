87c9d52aa5ffd6285c7f4aa52f7a533660587ef8
arpit1712@gmail.com <arpit1712@gmail.com@d1f48388-536f-61ed-b7e5-cd5581ae4938>
2011-03-22 00:15:40 +0000

    basic infra setup, need to multicast now

--- dos4/Client.java
+++ dos4/Client.java
@@ -1,26 +1,45 @@
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Client
+
+public class Client implements Runnable
 {
-	int mySequenceNum;	//number of times process has requested critical section
-	int[] sequenceVector;	//best known information about other processes
-	ClientConfig myConfig;	//my own sleepTime, opTime, name, etc.
-	boolean token;
-	Thread requesterThread,	//requester is used for multicast,
-	listenerThread,	//listener listens to other's multicast requests,
-	tokenThread;	//token sends and receives token through unicast.
-				
+	private int mySequenceNum;	//number of times process has requested critical section
+	private int numAccesses;
+	private int[] sequenceVector;	//best known information about other processes
+	private ClientConfig myConfig;	//my own sleepTime, opTime, name, etc.
+	private boolean token;
+	private String multiCastAddress;
+	private AtomicInteger state;	// 0 = multicast, 1 = listener, 2 = unicast
+	private int multiCastPort;
+	private MulticastSocket socket; 
 	
-	public Client(int numClients, ClientConfig config)
+	public Client(int myID)
 	{
-		mySequenceNum = 0;	//should be 0 initially
-		myConfig = config;
-		if(myConfig.getClientNum() == 1)	//initially only client 1 gets the token
+		if(myID == 1)	//initially only client 1 gets the token
 			token = true;
 		else
 			token = false;
-		sequenceVector = new int[numClients];
-		for (int i = 0; i < numClients; i++)
+
+		//read rest of the values from ConfigReader class
+		ConfigReader cr = new ConfigReader("system.properties");
+		multiCastAddress = cr.getMulticastAddress();
+		multiCastPort = cr.getMulticastPort();
+		numAccesses = cr.getNumAccesses();
+		myConfig = cr.getClientConfig(myID);
+		mySequenceNum = 0;	//should be 0 initially
+		int totalClients = cr.getNumClients();
+		sequenceVector = new int[totalClients];
+		for (int i = 0; i < totalClients; i++)
 			sequenceVector[i] = 0;
+		state = new AtomicInteger(0);
 	}
 	
 	public static void main(String[] args)
@@ -28,6 +47,76 @@ public class Client
 		if(args.length < 1)
 		{
 			System.err.println("Usage : java Client <ID_of_Client>");
+			System.exit(-1);
 		}
+		Client aClient = new Client(Integer.parseInt(args[0]));	//args[0] is cNum (client's ID)
+		aClient.setupMulticast();	//setup multicast address before starting either sending or receiving thread
+
+		//create 3 threads, according to the value of state, automatically threads will acquire  
+		//the role of multicast, listener or unicast
+		for(int i = 0; i < 3; i++)
+			new Thread(aClient).start();
+	}
+
+	private void setupMulticast()
+	{
+		try {
+			socket = new MulticastSocket(multiCastPort);
+			InetAddress group = InetAddress.getByName(multiCastAddress);
+			socket.joinGroup(group);
+		} catch (IOException ioex) {
+			// TODO Auto-generated catch block
+			ioex.printStackTrace();
+		}		
+	}
+
+	@Override
+	public void run()
+	{
+		if(state.getAndIncrement() == 0)
+		{
+			//request token through multicast if you don't have token already
+			if(!token)
+			{
+				StringBuffer requestMsg = new StringBuffer("arpit");
+				
+			}
+						
+			
+			byte[] data = new byte[1000];
+			try {
+				DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(multiCastAddress), multiCastPort);
+			} catch (UnknownHostException uhex)
+			{
+				System.err.println("No host with address " + multiCastAddress + ". **Exception : " + uhex.getMessage());
+			}
+		}
+		else if(state.getAndIncrement() == 1)
+		{
+			//listen to others request
+		}
+		else if(state.getAndIncrement() == 2)
+		{
+			//send and receive token through unicast
+		}
+		else
+		{
+			System.err.println("Failed to maintain \"state\". BAD!");
+		}
+			
+		
+	}
+}
+
+class Token implements Serializable
+{
+	private static final long serialVersionUID = 3043525584730649838L;
+	int[] tokenVector;
+	Queue<Integer> tokenQueue;
+	
+	public Token(int vectorSize)
+	{
+		tokenVector = new int[vectorSize];
+		tokenQueue = new ConcurrentLinkedQueue<Integer>();
 	}
 }
