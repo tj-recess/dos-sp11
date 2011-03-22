68ad2cf349a59e2c01f8f4c768da3e03ece6276b
arpit1712@gmail.com <arpit1712@gmail.com@d1f48388-536f-61ed-b7e5-cd5581ae4938>
2011-03-22 16:50:48 +0000

    ready to multicast

--- dos4/Client.java
+++ dos4/Client.java
@@ -1,4 +1,8 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -11,34 +15,33 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Client implements Runnable
 {
-	private int mySequenceNum;	//number of times process has requested critical section
+	private AtomicInteger mySequenceNum;	//number of times process has requested critical section
 	private int numAccesses;
 	private int[] sequenceVector;	//best known information about other processes
 	private ClientConfig myConfig;	//my own sleepTime, opTime, name, etc.
-	private boolean token;
 	private String multiCastAddress;
 	private AtomicInteger state;	// 0 = multicast, 1 = listener, 2 = unicast
 	private int multiCastPort;
-	private MulticastSocket socket; 
+	private MulticastSocket socket;
+	private Token myToken = null;
 	
 	public Client(int myID)
 	{
-		if(myID == 1)	//initially only client 1 gets the token
-			token = true;
-		else
-			token = false;
-
 		//read rest of the values from ConfigReader class
 		ConfigReader cr = new ConfigReader("system.properties");
 		multiCastAddress = cr.getMulticastAddress();
 		multiCastPort = cr.getMulticastPort();
 		numAccesses = cr.getNumAccesses();
 		myConfig = cr.getClientConfig(myID);
-		mySequenceNum = 0;	//should be 0 initially
+		mySequenceNum = new AtomicInteger(0);	//should be 0 initially
 		int totalClients = cr.getNumClients();
 		sequenceVector = new int[totalClients];
 		for (int i = 0; i < totalClients; i++)
 			sequenceVector[i] = 0;
+		if(myID == 1)	//initially only client 1 gets the token
+			myToken = new Token(totalClients);
+		else
+			myToken = null;
 		state = new AtomicInteger(0);
 	}
 	
@@ -76,41 +79,68 @@ public class Client implements Runnable
 		if(state.getAndIncrement() == 0)
 		{
 			//request token through multicast if you don't have token already
-			if(!token)
+			if(myToken == null)
 			{
-				StringBuffer requestMsg = new StringBuffer("arpit");
-				
-			}
-						
-			
-			byte[] data = new byte[1000];
-			try {
-				DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(multiCastAddress), multiCastPort);
-			} catch (UnknownHostException uhex)
-			{
-				System.err.println("No host with address " + multiCastAddress + ". **Exception : " + uhex.getMessage());
+				try {
+					myToken.tokenVector[myConfig.getClientNum() - 1] = mySequenceNum.incrementAndGet();
+					socket.setTimeToLive(1);
+					ByteArrayOutputStream bos = new ByteArrayOutputStream();
+					ObjectOutputStream oos = new ObjectOutputStream(bos);					
+					oos.writeObject(myToken);
+					byte[] data = bos.toByteArray();
+					DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(multiCastAddress), multiCastPort);
+					socket.send(packet);
+					System.out.println("Client" + myConfig.getClientNum() + " - DEBUG: a token sent");
+				}
+				catch (UnknownHostException uhex)
+				{
+					System.err.println("No host with address " + multiCastAddress + ". **Exception : " + uhex.getMessage());
+				}
+				catch (IOException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 		else if(state.getAndIncrement() == 1)
 		{
 			//listen to others request
+			byte[] buffer = new byte[10000];
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			try {
+				socket.receive(packet);
+				ByteArrayInputStream bis = new ByteArrayInputStream(buffer);
+				ObjectInputStream ois = new ObjectInputStream(bis);
+				Token receivedToken = (Token)ois.readObject();
+				if(receivedToken.myIdentifier.equals("arpit"));
+				{
+					//do something with token
+					System.out.println("Client" + myConfig.getClientNum() + " - DEBUG: proper token received");
+				}
+			} catch (IOException e)
+			{
+				System.err.println("Error receiving datagram packet. **Exception = " + e.getMessage());
+			} catch (ClassNotFoundException e) {
+				System.err.println("Unknown object received! **Exception = " + e.getMessage());
+			}			
 		}
 		else if(state.getAndIncrement() == 2)
 		{
 			//send and receive token through unicast
+			
 		}
 		else
 		{
 			System.err.println("Failed to maintain \"state\". BAD!");
 		}
-			
-		
 	}
 }
 
 class Token implements Serializable
 {
 	private static final long serialVersionUID = 3043525584730649838L;
+	String myIdentifier = "arpit";
 	int[] tokenVector;
 	Queue<Integer> tokenQueue;
 	
@@ -118,5 +148,11 @@ class Token implements Serializable
 	{
 		tokenVector = new int[vectorSize];
 		tokenQueue = new ConcurrentLinkedQueue<Integer>();
+	}
+	public Token(int vectorSize, String identifier)
+	{
+		tokenVector = new int[vectorSize];
+		tokenQueue = new ConcurrentLinkedQueue<Integer>();
+		myIdentifier = identifier;
 	}
 }
