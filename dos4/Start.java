679ad24083db80127d80adbac7dfa6531028a84c
arpit1712@gmail.com <arpit1712@gmail.com@d1f48388-536f-61ed-b7e5-cd5581ae4938>
2011-03-31 22:03:17 +0000

    suppressed all output

--- dos4/Start.java
+++ dos4/Start.java
@@ -27,7 +27,7 @@ public class Start
 			try 
 			{
 				ClientConfig aClient= cr.getClientConfig(i);
-				aClient.print();	//DEBUG
+//				aClient.print();	//DEBUG
 				Process remote = Runtime.getRuntime().exec("ssh " + aClient.getAddress() + " cd " + path + " ; java Client " + aClient.getClientNum());
 				//new Thread(new ClientOutputStreamReader(remote, aClient.getAddress(), "input")).start();
 				new Thread(new ClientOutputStreamReader(remote, aClient.getAddress(), "error")).start();
@@ -72,14 +72,14 @@ public class Start
 				while((output = clientReader.readLine()) != null)
 				synchronized(ClientOutputStreamReader.class)
 				{
-					System.out.println(prefix + output);
+					//System.out.println(prefix + output);
 				}
 				
 				int exitVal = 0;
 				try {exitVal = clientProcID.waitFor();}
 				catch (InterruptedException e) {/*Ignore*/}
 				Runtime.getRuntime().exec("ssh " + clientName + " skill java");
-				System.out.println(prefix + "died!");
+				//System.out.println(prefix + "died!");
 				if(exitVal != 0)
 					Runtime.getRuntime().exec("skill java");
 			}
